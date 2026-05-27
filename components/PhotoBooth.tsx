675a4223d80ac04644188e4a5173c14e03d4f609
niyikes <arunya.raghavan@gmail.com>
2026-05-27 10:59:32 +0530

    fix strip download bug

--- components/PhotoBooth.tsx
+++ components/PhotoBooth.tsx
@@ -436,6 +436,7 @@ const cx = out.width / 2
     const date = `${now.getFullYear()}${String(now.getMonth()+1).padStart(2,'0')}${String(now.getDate()).padStart(2,'0')}`
     const time = `${String(now.getHours()).padStart(2,'0')}${String(now.getMinutes()).padStart(2,'0')}`
     link.download = `eiko-${date}-${time}.jpg`
+    link.click()
 
   }, [frames])
 
